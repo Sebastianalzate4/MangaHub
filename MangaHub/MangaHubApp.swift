680b835f3e3197b2c5013f5a23fa5cea93ddf02f
Sebastian Alzate <sebastiansa7@icloud.com>
2024-07-11 17:24:03 -0500

    Initial Commit

---
+++ MangaHub/MangaHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaHubApp.swift
+//  MangaHub
+//
+//  Created by Sebastian Alzate on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MangaHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
